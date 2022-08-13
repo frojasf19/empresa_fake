89ad68287a11e0a434b4a208bbc91cb352309732
fernando <frojasf19@gmail.com>
2022-08-13 12:25:22 -0400

    post

--- routers/administrativos.js
+++ routers/administrativos.js
@@ -17,7 +17,20 @@ routerAdministrativos.get('/:sexo', (req, res)=>{
     if(resp.length === 0) return res.status(404).json('No se encontraron empleados de ese sexo')
     return res.status(200).json(resp)
 })
+routerAdministrativos.get('/:sexo/:edad', (req, res)=>{
+    const sexo = req.params.sexo
+    const edad = req.params.edad
+    const resp = administrativos.filter(e => e.sexo === sexo && e.edad == edad)
+    if(resp.length === 0) return res.status(404).json('No se encontraron empleados de ese sexo')
+    return res.status(200).json(resp)
+})
 
+routerAdministrativos.post('/', (req, res)=>{
+    const nE = req.body
+    if(nE.id == undefined || nE.nombre == undefined || nE.edad == undefined || nE.sexo == undefined || nE.correo == undefined) return res.status(404).json('No se pudo registrar al nuevo empleado')
+    administrativos.push(nE)
+    return res.status(200).json('Empleado registrado con exito')
+})
 routerAdministrativos
 
 
